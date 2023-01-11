c6616776a43f573eb3c562827b181ae076e3ce9e
nukelet <nukelet@protonmail.com>
2023-01-11 14:30:01 -0300

    Set up initial project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="cpos",
+      version="0.1",
+      packages=["cpos",
+                "cpos.p2p",
+                "cpos.test"],
+      scripts=[],
+      package_data={},
+      )
