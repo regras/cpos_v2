5a79c5b0c3a148773f9c52c9017415de126117c1
Vinicius Peixoto <nukelet@protonmail.com>
2023-04-11 16:06:57 -0300

    [p2p] Implement first version of p2p network interface

--- cpos/p2p/peer.py
+++ cpos/p2p/peer.py
@@ -1,8 +1,5 @@
-class PeerNetInfo:
-    def __init__(self):
-        self.ip
-        pass
-
 class Peer:
-    def __init__(self):
-        pass
+    def __init__(self, peer_id, peer_ip, peer_port):
+        self.identifier = peer_id
+        self.ip = peer_ip
+        self.port= peer_port
