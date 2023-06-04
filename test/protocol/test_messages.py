920bc1166fb7a81eb1069fef8431dd12985d3712
nukelet <nukelet@protonmail.com>
2023-06-04 19:15:32 -0300

    [test/protocol] Add test for BlockBroadcast serialization

--- test/protocol/test_messages.py
+++ test/protocol/test_messages.py
@@ -6,7 +6,6 @@ import pytest
 def test_hello_serialization():
     assert True
 
-@pytest.mark.skip(reason="unimplemented")
 def test_block_broadcast_serialization():
     gen = GenesisBlock()
     transactions = TransactionList()
@@ -16,12 +15,18 @@ def test_block_broadcast_serialization():
               round = 1,
               index = 1,
               ticket_number = 1)
-    block = Block(parent_hash = b.hash,
+    original = Block(parent_hash = b.hash,
                   transactions = transactions,
                   owner_pubkey = b"testkey2",
                   round = 2,
                   index = 2,
                   ticket_number = 1)
-    msg = BlockBroadcast(block)
-    print(msg.serialize())
-    assert False
+    msg = BlockBroadcast(original)
+    deserialized = BlockBroadcast.deserialize(msg.serialize()).block
+    assert original.hash == deserialized.hash
+    assert original.parent_hash == deserialized.parent_hash
+    assert original.owner_pubkey == deserialized.owner_pubkey
+    assert original.transaction_hash == deserialized.transaction_hash
+    assert original.round == deserialized.round
+    assert original.index == deserialized.index
+    assert original.ticket_number == deserialized.ticket_number
