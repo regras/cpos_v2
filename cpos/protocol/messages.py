3385bb951c260692b6adb84c7b9f2e218fdad2c6
Vinicius Peixoto <nukelet@protonmail.com>
2023-06-06 22:45:24 -0300

    [protocol/messages] Serialize messages using pickle

--- cpos/protocol/messages.py
+++ cpos/protocol/messages.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 import json
 from base64 import b64encode, b64decode
+import pickle
+from typing import Self
 
 from cpos.core.block import Block
 from cpos.core.transactions import TransactionList
@@ -9,6 +11,8 @@ class MessageCode:
     UNDEFINED = 0x0
     HELLO = 0x1
     BLOCK_BROADCAST = 0x2
+    PEER_LIST_REQUEST = 0x3
+    PEER_LIST = 0x4
 
 class MessageParseError(Exception):
     pass
@@ -21,72 +25,33 @@ class Message:
         pass
 
     def serialize(self) -> bytes:
-        raise NotImplementedError
+        return pickle.dumps(self, protocol=pickle.HIGHEST_PROTOCOL)
 
     @classmethod
-    def deserialize(cls, raw) -> Message:
-        raise NotImplementedError
+    def deserialize(cls, raw) -> Self:
+        return pickle.loads(raw)
 
 
 class Hello(Message):
     def __init__(self, peer_id: bytes, peer_port: int | str):
-        self.msg_code = MessageCode.HELLO
+        self.code = MessageCode.HELLO
         self.peer_id = peer_id
         self.peer_port = peer_port
 
-    def serialize(self) -> bytes:
-        data = {}
-        data["peer_id"] = b64encode(self.peer_id).decode("ascii")
-        data["peer_port"] = self.peer_port
-        return bytes(json.dumps(data), "ascii")
-
-    @classmethod
-    def deserialize(cls, raw: bytes) -> Hello:
-        raw_dict = json.loads(raw.decode("ascii"))
-        peer_id = b64decode(raw_dict["peer_id"])
-        peer_port = raw_dict["peer_port"]
-        msg = Hello(peer_id, peer_port)
-        return msg
-
     def __str__(self):
-        return f"Hello(id={self.peer_id}, port={self.peer_port})"
+        return f"Hello(id={self.peer_id.hex()[0:8]}, port={self.peer_port})"
 
 class BlockBroadcast(Message):
     def __init__(self, block: Block):
         self.code = MessageCode.BLOCK_BROADCAST
         self.block = block
 
-    def serialize(self) -> bytes:
-        # TODO: this is horribly ugly, we need to find a decent serialization strategy
-        fields = ["hash", "parent_hash", "transaction_hash", "owner_pubkey", "signed_node_hash", "index", "round", "ticket_number"]
-        b = self.block
-        data = {}
-        for field in fields:
-            entry = b.__dict__[field]
-            if isinstance(entry, bytes):
-                data[field] = b64encode(entry).decode("ascii")
-            else:
-                data[field] = entry
-        return bytes(json.dumps(data), 'ascii')
-    
-    @classmethod
-    def deserialize(cls, raw: bytes) -> BlockBroadcast:
-        fields = ["hash", "parent_hash", "transaction_hash", "owner_pubkey", "index", "round", "ticket_number"]
-        raw_dict = json.loads(raw.decode("ascii"))
-        print(f"deserialized: {raw_dict}")
-        # TODO: this transaction stub needs to be implemented eventually
-        stub = TransactionList()
-        parent_hash = b64decode(raw_dict["parent_hash"])
-        owner_pubkey = b64decode(raw_dict["owner_pubkey"])
-        signed_node_hash = b64decode(raw_dict["signed_node_hash"])
-        block = Block(parent_hash = parent_hash,
-                      transactions = stub,
-                      owner_pubkey = owner_pubkey,
-                      signed_node_hash = signed_node_hash,
-                      round = raw_dict["round"],
-                      index = raw_dict["index"],
-                      ticket_number = raw_dict["ticket_number"])
-        return BlockBroadcast(block)
-        # except Exception as e:
-        #     print(e)
-        #     raise ValueError("invalid block serialization")
+class PeerListRequest(Message):
+    def __init__(self, node_id: bytes):
+        self.code = MessageCode.PEER_LIST_REQUEST
+        self.node_id = node_id
+
+class PeerList(Message):
+    def __init__(self, peerlist: list[tuple[str, str | int, bytes]]):
+        self.code = MessageCode.PEER_LIST
+        self.peerlist = peerlist
