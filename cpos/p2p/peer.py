6b0db95d3f67dfb70cca5cd14ad7c73bc56836c7
nukelet <nukelet@protonmail.com>
2023-06-12 13:43:01 -0300

    [p2p/discovery] Implement peer discovery service

--- cpos/p2p/peer.py
+++ cpos/p2p/peer.py
@@ -1,5 +1,16 @@
+from __future__ import annotations
+
 class Peer:
-    def __init__(self, peer_id, peer_ip, peer_port):
-        self.identifier = peer_id
+    def __init__(self, peer_ip: str, peer_port: int, peer_id: bytes):
         self.ip = peer_ip
         self.port= peer_port
+        self.id = peer_id
+    
+    def __str__(self):
+        return f"({self.ip}:{self.port}, {self.id.hex()[0:8]})"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, peer: Peer):
+        return self.id == peer.id
