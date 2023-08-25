03c7fb37bf12821f4143e8468e48726373103701
nukelet <nukelet@protonmail.com>
2023-08-25 06:56:25 -0300

    [cpos/p2p/discovery] Implement an instant reply mode for the beacon

--- cpos/p2p/discovery/beacon.py
+++ cpos/p2p/discovery/beacon.py
@@ -6,7 +6,7 @@ from cpos.p2p.peer import Peer
 from cpos.p2p.discovery.messages import Message, MessageCode, Hello, PeerList
 
 class Beacon:
-    def __init__(self, port: int):
+    def __init__(self, port: int, instant_reply: bool = True):
         logger = logging.getLogger(__name__)
         handler = logging.StreamHandler()
         formatter = logging.Formatter(f"[%(asctime)s][%(levelname)s] {__name__} %(message)s")
@@ -24,6 +24,7 @@ class Beacon:
         self.logger.info(f"Listening on port {self.port}")
 
         self.peers: list[Peer] = []
+        self.instant_reply = instant_reply
 
     def start(self):
         self.listen()
@@ -40,8 +41,6 @@ class Beacon:
                 self.logger.error(f"failed to deserialize message from {addr}:{port}, reason: {e}")
                 continue
 
-
-            
             self.logger.debug(f"handling message: {msg}")
             if isinstance(msg, Hello):
                 peer = Peer(addr, msg.port, msg.id)
@@ -49,13 +48,14 @@ class Beacon:
                     self.peers.append(peer)
                     self.logger.info(f"registering new peer ({peer.id.hex()[0:8]}, {peer.ip}:{peer.port})")
 
-            reply = PeerList(self.peers)
-            self.logger.info(f"sending peerlist to ({peer.id.hex()[0:8]}, {peer.ip}:{peer.port}): {self.peers}")
-            try:
-                sent = self.socket.sendto(reply.serialize(), (addr, port))
-                self.logger.debug(f"sent {sent} bytes to ({peer.id.hex()[0:8]}, {peer.ip}:{peer.port}): {self.peers}")
-            except Exception as e:
-                self.logger.error(f"unable to send reply to {peer}: {e}")
+            if self.instant_reply:
+                reply = PeerList(self.peers)
+                self.logger.info(f"sending peerlist to ({peer.id.hex()[0:8]}, {peer.ip}:{peer.port}): {self.peers}")
+                try:
+                    sent = self.socket.sendto(reply.serialize(), (addr, port))
+                    self.logger.debug(f"sent {sent} bytes to ({peer.id.hex()[0:8]}, {peer.ip}:{peer.port}): {self.peers}")
+                except Exception as e:
+                    self.logger.error(f"unable to send reply to {peer}: {e}")
 
 
 
